09e987b09ac5cb55d289c6822c8a6d4141b313a8
majo06002 <Max-Lukas@outlook.com>
2023-10-18 10:41:05 +0200

    Add project files.

---
+++ Inämningsupgift 4.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Inlämningsupgift4_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett heltal");
+            int tal = int.Parse(Console.ReadLine());
+            int störst = tal;
+
+            string svar = "";
+            while (svar != "n") 
+            {
+                Console.WriteLine("vill du skriva ett till heltal? j/n");
+                svar = Console.ReadLine();   
+                if (svar == "n")
+                {
+                    break;
+                }
+                Console.WriteLine("Skriv in ett till heltal");
+                int nytttal= int.Parse(Console.ReadLine());
+
+                if(nytttal > störst)
+                {
+                    störst = nytttal;
+                }
+
+
+
+
+
+
+            }
+            Console.WriteLine("Det största talet du skrev var " + störst);
+           
+            
+        }
+    }
+}
